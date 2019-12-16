33984caa171539ba2b7ac089449bfc78ff3e3685
sali <salialmurshidi@gmail.com>
2019-12-16 12:22:27 +0100

    add alert to ask the user if he or she wants cake and get the answer to his or her choise

---
+++ 01-base/03-prompt-if/script.js
@@ -0,0 +1,9 @@
+(function() {
+
+    // your code here
+    if(confirm("Do you want some cake ?")){
+        alert("congratulate")
+    }else {
+        alert("More cake for me then :p !");
+    }
+})();
