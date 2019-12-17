39c42890b068de4cd36c9a928fae239bde13e723
sali <salialmurshidi@gmail.com>
2019-12-17 10:37:05 +0100

    add files for the 2.3 task

---
+++ 02-maths/03-even-square/script.js
@@ -0,0 +1,12 @@
+(function() {
+
+    document.getElementById("run").addEventListener("click", function() {
+
+        // your code here
+        for(num = 1 ; num <= 21){
+
+        }
+
+    });
+
+})();
