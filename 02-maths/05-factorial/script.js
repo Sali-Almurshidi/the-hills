0f69e4b6c21f422f4e7d5133fd08f964c117fbfd
sali <salialmurshidi@gmail.com>
2019-12-17 15:04:00 +0100

    add files to 5 task

---
+++ 02-maths/05-factorial/script.js
@@ -0,0 +1,11 @@
+(function() {
+
+    // to get the value of an input: document.getElementById("element-id").value
+
+    document.getElementById("run").addEventListener("click", function() {
+
+        // your code here
+
+    });
+
+})();
