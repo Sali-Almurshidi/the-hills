2dbbdfeae9926463b69057e9eebeb99d608b47eb
sali <salialmurshidi@gmail.com>
2019-12-17 15:09:08 +0100

    add the code to calcoulator the factorial number

--- 02-maths/05-factorial/script.js
+++ 02-maths/05-factorial/script.js
@@ -5,6 +5,16 @@
     document.getElementById("run").addEventListener("click", function() {
 
         // your code here
+        let randomValue = document.getElementById("number").value;
+        let factorial = 1 ;
+        let num ;
+
+        for(num = randomValue ; num >= 1; num--){
+            factorial *= num ;
+        }
+
+        alert("The result of number : " + randomValue + " is " + factorial);
+
 
     });
 
