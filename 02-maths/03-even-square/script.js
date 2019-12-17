0817ccb4117285bc012261e84d5d3ed9810da34a
sali <salialmurshidi@gmail.com>
2019-12-17 14:35:19 +0100

    print the square of input number as alert

--- 02-maths/03-even-square/script.js
+++ 02-maths/03-even-square/script.js
@@ -1,16 +1,16 @@
-(function() {
+(function test() {
 
     document.getElementById("run").addEventListener("click", function() {
 
         // your code here
         let square = new Array();
+        let num = 0 ;
+        let square_number =0 ;
         for(num = 1 ; num <= 21 ; num++){
-            num*= num;
-            console.
-            square.add(num);
-            num ++ ;
+            square_number =num * num ;
+            alert (square_number);
+           // square.add(square_number);
         }
-
     });
 
 })();
