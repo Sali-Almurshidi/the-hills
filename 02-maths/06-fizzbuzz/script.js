e890b2d99a4aad45414ab493f96ee9bb30f2a072
sali <salialmurshidi@gmail.com>
2019-12-18 10:43:24 +0100

    fix the 2.6 task output by change the if condition place

--- 02-maths/06-fizzbuzz/script.js
+++ 02-maths/06-fizzbuzz/script.js
@@ -1,5 +1,27 @@
 (function() {
 
     // your code here
+    let multiple3;
+    let multiple5;
+    let num =1 ;
+    for(num  ; num <= 100 ; num++){
+
+        multiple3 =num % 3;
+        multiple5 =num % 5;
+
+        if((multiple3 == 0 ) && (multiple5 == 0)){
+            console.log(num + "fizzbuzz");
+
+        }else if(multiple5 == 0){
+            console.log(num + "buzz");
+
+        }else if(multiple3 == 0 ){
+            console.log(num + "fizz");
+
+        }else{
+            console.log(num + "simply");
+        }
+
+    }
 
 })();
