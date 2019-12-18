215a47eacd9febb601bf9e6b4557ff966855d1ad
sali <salialmurshidi@gmail.com>
2019-12-18 09:35:07 +0100

    change the background color by use two way , 1 use switch , 2 use 4 function and call every one in it own onclick

--- 03-colors/01-change-bcg-one/script.js
+++ 03-colors/01-change-bcg-one/script.js
@@ -1,5 +1,42 @@
-(function ChangeColor(buttonClik) {
+
+function changeColor (color_choose) {
 
     // your code here
+    switch (color_choose) {
 
-})();
+        case  "red":
+            document.body.style.backgroundColor = "red";
+            alert("red");
+            break;
+        case "green":
+            document.body.style.backgroundColor = "green";
+            alert("green");
+            break;
+        case "yellow":
+            document.body.style.backgroundColor = "yellow";
+            alert("yellow");
+            break;
+        case "blue":
+            document.body.style.backgroundColor = "blue";
+            alert("blue");
+            break;
+    }
+
+
+}
+
+/* function myRed() {
+    document.body.style.backgroundColor = "red";
+}
+
+function myGreen() {
+    document.body.style.backgroundColor = "green";
+}
+
+function myYellow() {
+    document.body.style.backgroundColor = "yellow";
+}
+
+function myBlue() {
+    document.body.style.backgroundColor = "blue";
+}  */
