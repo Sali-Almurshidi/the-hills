c8d5109bcfbbc79b0629d365e6cd007864d75ecc
sali <salialmurshidi@gmail.com>
2019-12-18 16:13:58 +0100

    done with 4.1 by call Date object

--- 04-dates/01-title-by-hour-one/script.js
+++ 04-dates/01-title-by-hour-one/script.js
@@ -3,7 +3,13 @@
     // to change the content of a tag: document.getElementById("element-id").innerHTML = "new-value"
 
     // your code here
-
-
+    var d = new Date();
+    let hour = d.getHours();
+    // alert(hour)
+     if( hour < 18){
+         document.getElementById('target').innerHTML = "Hello";
+     }else {
+         document.getElementById('target').innerHTML = "Good evening";
+     }
 
 })();
