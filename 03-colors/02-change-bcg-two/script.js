9ab6e5509cafc027495a8fab118dad1063f1b174
sali <salialmurshidi@gmail.com>
2019-12-18 10:14:04 +0100

    get the input from the user then send it to the function to put it as the background

--- 03-colors/02-change-bcg-two/script.js
+++ 03-colors/02-change-bcg-two/script.js
@@ -1,5 +1,10 @@
 (function(){
 
-
+    document.getElementById('run').addEventListener('click',function () {
+        let colorInput = document.getElementById('color').value;
+        alert(colorInput + " in side listener");
+        document.body.style.backgroundColor = colorInput;
+        alert("done");
+    });
 
 })();
