556c8928e8c9762ac1e5f3565d4426f2e2ae04a7
sali <salialmurshidi@gmail.com>
2019-12-18 17:00:02 +0100

    dane with 4.3 task

--- 04-dates/03-age-by-select/script.js
+++ 04-dates/03-age-by-select/script.js
@@ -1,5 +1,14 @@
 (function() {
 
     // your code here
+    document.getElementById('run').addEventListener('click',function () {
+        let userDay = document.getElementById('dob-day').value;
+        let userMonth = document.getElementById('dob-month').value;
+        let userYear = document.getElementById('dob-year').value;
+        let userAge = 2019 - userYear ;
+        let userAgeMonth = 12 - userMonth ;
+
+        alert("user birthdate is "+userYear+"/"+userMonth+"/"+userDay+ "and his/her age is  "+userAge + " years and "+ userAgeMonth + " month");
+    });
 
 })();
