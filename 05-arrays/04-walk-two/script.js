709e23024311165d598b3f0620033b2e46e321c6
sali <salialmurshidi@gmail.com>
2019-12-19 15:45:53 +0100

    done with 5.4

---
+++ 05-arrays/04-walk-two/script.js
@@ -0,0 +1,25 @@
+(function() {
+
+    var fruits = [
+        "pomme",
+        "poire",
+        "fraise",
+        "tomate",
+        "kiwi",
+        "banane",
+        "orange",
+        "mandarine",
+        "durian",
+        "pêche",
+        "raisin",
+        "cerise",
+    ];
+
+    // your code here
+    fruits.forEach(myFunction);
+
+})();
+
+function myFunction(item, index) {
+    console.log(index + " " + item);
+}
