8bf9316212fa48db402220fecb7ee9042b78d1fa
sali <salialmurshidi@gmail.com>
2019-12-19 15:31:39 +0100

    add 5.3 files

---
+++ 05-arrays/03-walk-one/script.js
@@ -0,0 +1,23 @@
+(function() {
+
+    var fruits = [
+        "apple",
+        "pear",
+        "raspberry",
+        "tomatos",
+        "kiwi",
+        "banana",
+        "orange",
+        "mandarin",
+        "durian",
+        "peache",
+        "grap",
+        "cherry",
+    ];
+
+    // your code here
+    document.getElementById('run').addEventListener('click',function () {
+
+    });
+
+})();
