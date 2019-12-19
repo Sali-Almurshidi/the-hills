87affb49a622589991748d6f33f529e8b3739467
sali <salialmurshidi@gmail.com>
2019-12-19 12:17:04 +0100

    add files to start taask 5.1  In the name of Allah

---
+++ 05-arrays/01-get-element/script.js
@@ -0,0 +1,20 @@
+(function() {
+
+    var fruits = [
+        "apple",
+        "pear",
+        "raspberry",
+        "tomatos",
+        "kiwi",
+        "banana",
+        "orange",
+        "mandarin",
+        "durian",
+        "peache",
+        "grap",
+        "cherry",
+    ];
+
+    // your code here
+
+})();
