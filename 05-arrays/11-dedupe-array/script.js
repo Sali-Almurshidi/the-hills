f3861940f2dc3f77ec57244ffa89d9bbbb2474d4
sali <salialmurshidi@gmail.com>
2019-12-20 11:30:12 +0100

    add files to start 5.11

---
+++ 05-arrays/11-dedupe-array/script.js
@@ -0,0 +1,22 @@
+(function() {
+
+    var fruits = [
+        "cerise",
+        "durian",
+        "pomme",
+        "poire",
+        "fraise",
+        "tomate",
+        "orange",
+        "mandarine",
+        "fraise",
+        "durian",
+        "pêche",
+        "cerise",
+        "raisin",
+        "cerise",
+    ];
+
+    // your code here
+
+})();
