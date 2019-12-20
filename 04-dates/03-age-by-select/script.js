42b667c6c5554e627be5c1ec6df328e6bd693a00
sali <salialmurshidi@gmail.com>
2019-12-20 10:58:36 +0100

    now you can see your age just the year

--- 04-dates/03-age-by-select/script.js
+++ 04-dates/03-age-by-select/script.js
@@ -9,17 +9,9 @@
 
         let currentDate = new Date();
         let userAge = currentDate.getFullYear() - userYear ;
-        let userAgeMonth  ;
+        let userAgeMonth ;
         let userAgeDay;
 
-        // this is for the days all the months has 30 day :P
-        if (userAgeDay <= currentDate.getDate()){
-            userAgeDay = currentDate.getDate() - userDay ;
-        }else if (userAgeDay > currentDate.getDate()){
-            userAgeDay = 30 - userDay ;
-        }else{
-            userAgeDay = 0 ;
-        }
 
         // this is for year
         if ( currentDate.getMonth() < (userMonth - 1))
@@ -30,16 +22,7 @@
         {
             userAge--;
         }
-
-        // this is for month
-        if (currentDate.getMonth() >= userMonth)
-            userAgeMonth = currentDate.getMonth() - userMonth;
-        else {
-            userAgeMonth = 12 + currentDate.getMonth() -userMonth;
-        }
-
-
-        alert("user birthdate is "+userYear+"/"+userMonth+"/"+userDay+ "and his/her age is  "+userAge + " years and "+ userAgeMonth + " month " + userAgeDay + " day " );
+        alert("user birthdate is "+userYear+ " year" );
     });
 
 })();
