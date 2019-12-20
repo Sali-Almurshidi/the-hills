2537b2a620ae5e33b0881c3575814eef80a5350b
sali <salialmurshidi@gmail.com>
2019-12-20 09:08:49 +0100

    start with 5.8

---
+++ 05-arrays/08-array-includes/script.js
@@ -0,0 +1,20 @@
+(function() {
+
+    var fruits = [
+        "apple",
+        "perry",
+        "strawberry",
+        "tomato",
+        "kiwi",
+        "banana",
+        "orange",
+        "mandarin",
+        "durian",
+        "peach",
+        "grapes",
+        "cherry",
+    ];
+
+    // your code here
+
+})();
