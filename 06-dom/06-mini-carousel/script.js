191e304199ba4cbad1f213bbbaeb3bd945ceb3b5
sali <salialmurshidi@gmail.com>
2019-12-23 16:41:43 +0100

    there is a problem with the img

---
+++ 06-dom/06-mini-carousel/script.js
@@ -0,0 +1,13 @@
+(function() {
+
+    var gallery= [
+        "../../_shared/img/bell.svg",
+        "../../_shared/img/clock.svg",
+        "../../_shared/img/compass.svg",
+        "../../_shared/img/lemon.svg",
+        "../../_shared/img/map.svg",
+    ];
+
+    // your code here
+
+})();
