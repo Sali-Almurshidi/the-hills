14b146a62b135ec0cab659b99f162cfba61fd1f1
sali <salialmurshidi@gmail.com>
2019-12-23 10:42:52 +0100

    add index next the class name

---
+++ 06-dom/03-select-three/script.js
@@ -0,0 +1,9 @@
+(function() {
+
+    document.getElementsByClassName("target")[0].innerHTML = "owned";
+    document.getElementsByClassName("target")[1].innerHTML = "owned";
+    document.getElementsByClassName("target")[2].innerHTML = "owned";
+    document.getElementsByClassName("target")[3].innerHTML = "owned";
+    document.getElementsByClassName("target")[4].innerHTML = "owned";
+
+})();
