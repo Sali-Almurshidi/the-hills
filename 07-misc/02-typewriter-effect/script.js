d2483613b3327b2fcbd82c7edb14a11cd3f9b9d3
sali <salialmurshidi@gmail.com>
2020-01-02 11:59:39 +0100

    done wih 7.2

--- 07-misc/02-typewriter-effect/script.js
+++ 07-misc/02-typewriter-effect/script.js
@@ -13,4 +13,21 @@
 
     // your code here
 
+    let i = 0;
+    // to get the txt from the p
+    let txt = document.getElementById("target").innerText; /* The text */
+    // make the p empty to add a new text inside it
+    target.innerHTML= "";
+    // to call the typeWriter function
+
+    typeWriter();
+    function typeWriter() {
+        if (i < txt.length) {
+            document.getElementById("target").innerHTML += txt.charAt(i);
+            i++;
+            // make time out between 1 and 1000
+            setTimeout(typeWriter, Math.floor(Math.random() * 1000) + 1);
+        }
+    }
 })();
+
