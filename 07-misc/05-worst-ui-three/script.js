dd419bd73e853be6aced69a74063e2e7fc5003b3
sali <salialmurshidi@gmail.com>
2020-01-03 10:54:46 +0100

    not work i have think with anther soulation

--- 07-misc/05-worst-ui-three/script.js
+++ 07-misc/05-worst-ui-three/script.js
@@ -1,16 +1,90 @@
-/* becode/javascript
- *
- * /07-misc/05-worst-ui-three/script.js - 7.5: la pire interface (3) : phone slot
- *
- * coded by leny@BeCode
- * started at 26/10/2018
- */
 
-// NOTE: don't focus on the existing code structure for now.
-// You will have time to focus on it later.
 
 (function() {
 
+    let result = document.getElementById("target").innerText;
     // your code here
+    // to start count
+    let partOneCount = 460;
+    let partTwoCount = 0;
+    let partThreeCount = 0;
+    let partFourCount = 0;
+
+    // default value
+    let resultPart1 = "460" ;
+    let resultPart2 = "00" ;
+    let resultPart3 = "00";
+    let resultPart4 = "00";
+
+    // call functions
+    document.getElementById("part-two").innerHTML= "";
+    animateValue("part-two");
+
+
+
+
+   // document.getElementById("part-one").innerText =
+
+    // data-min="460" data-max="499"
+    document.getElementById("fix-part-one").addEventListener("click" , function () {
+        partOneCount++;
+        resultPart1 = partOneCount ;
+        if(resultPart1 > 499){
+            resultPart1 = "460" ;
+        }
+        document.getElementById("target").innerHTML = "+" + resultPart1 + resultPart2 + resultPart3 + resultPart4 ;
+    });
+
+    //data-min="00" data-max="99"
+    document.getElementById("fix-part-two").addEventListener("click" , function () {
+
+        document.getElementById("target").innerHTML = "+" + resultPart1 + resultPart2 + resultPart3 + resultPart4 ;
+    });
+
+    //data-min="00" data-max="99"
+    document.getElementById("fix-part-three").addEventListener("click" , function () {
+        partThreeCount++;
+        resultPart3 = partThreeCount ;
+        if (partThreeCount <= 9 ){
+            resultPart3 = "0" + partThreeCount ;
+        }else if(partFourCount > 99){
+            resultPart3 = "00";
+        }else {
+            resultPart3 =  partThreeCount ;
+        }
+        document.getElementById("target").innerHTML = "+" + resultPart1 + resultPart2 + resultPart3 + resultPart4 ;
+    });
+
+    //data-min="00" data-max="99"
+    document.getElementById("fix-part-four").addEventListener("click" , function () {
+        partFourCount++;
+        resultPart4 = partFourCount ;
+        if (partFourCount <= 9 ){
+            resultPart4 = "0" + partFourCount ;
+        }else if(partFourCount > 99){
+            resultPart4 = "00";
+        }
+        else {
+            resultPart4 = partFourCount ;
+        }
+        document.getElementById("target").innerHTML = "+" + resultPart1 + resultPart2 + resultPart3 + resultPart4 ;
+    });
 
 })();
+
+function animateValue() {
+    let range = 99;
+    let current = 0;
+    let increment = 100 > 0? 1 : -1;
+    let stepTime = Math.abs(Math.floor(5000 / range));
+    //var obj = document.getElementById(id);
+    let timer = setInterval(function() {
+        current += increment;
+        document.getElementById("part-two").innerHTML= current;
+        if (current == 99) {
+            clearInterval(timer);
+        }
+    }, stepTime);
+}
+
+
