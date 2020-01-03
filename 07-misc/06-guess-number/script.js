1fa89f355f94cd6ea68d7f1067e0f960e9b868e4
sali <salialmurshidi@gmail.com>
2020-01-03 12:22:49 +0100

    done with this one , it is easy

--- 07-misc/06-guess-number/script.js
+++ 07-misc/06-guess-number/script.js
@@ -10,7 +10,19 @@
 // You will have time to focus on it later.
 
 (function() {
-
+    let randomNumber = Math.floor(Math.random()* 100 +1);
+    console.log(randomNumber);
     // your code here
+    document.getElementById("idButton").addEventListener("click",function () {
+        let gussNumber = document.getElementById("idGuessNumber").value;
+        console.log(gussNumber);
+        if(gussNumber == randomNumber){
+            document.getElementById("idResult").innerHTML = "you get it";
+        }else if(gussNumber < randomNumber){
+            document.getElementById("idResult").innerHTML = "hiiiiiiigher";
+        }else if(gussNumber > randomNumber){
+            document.getElementById("idResult").innerHTML = "loooooower";
+        }
+    });
 
 })();
