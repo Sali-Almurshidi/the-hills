8a98ce6469e75c8f3d547048d76524b2651d84df
sali <salialmurshidi@gmail.com>
2020-01-07 15:10:06 +0100

    add the element by push

--- 09-fetch/04-add/script.js
+++ 09-fetch/04-add/script.js
@@ -1,14 +1,33 @@
-/* becode/javascript
- *
- * /09-fetch/04-add/script.js - 11.4: ajouter un élément
- *
- * coded by leny@BeCode
- * started at 12/05/2019
- */
+let elementData;
+fetch("/api.json").then(response => {
+    return response.json();
+}).then(function (data) {
+    elementData = data.heroes;
+}).catch(err => {
+    console.log("Error");
+})
 
-// NOTE: don't focus on the existing code structure for now.
-// You will have time to focus on it later.
+document.getElementById("run").addEventListener("click", function () {
+    let name = document.getElementById("hero-name").value;
+    let alterEgo = document.getElementById("hero-alter-ego").value;
+    let powers = document.getElementById("hero-powers").value;
+    console.log(name + alterEgo + powers + "in event listener");
+    // function to add all the
+    addNewElement(name, alterEgo, powers);
 
-(() => {
-    // your code here
-})();
+}); // add events listener to get the input elements
+
+function addNewElement(heroName, heroAlert, heroPowers) {
+
+    let laseID = elementData.length;
+    console.log(laseID + " elements length");
+
+    elementData.push({
+        id: laseID+1,
+        name: heroName,
+        alterEgo: heroAlert,
+        abilities: heroPowers
+    });
+    console.log(elementData);
+
+}// push the new element to the array
