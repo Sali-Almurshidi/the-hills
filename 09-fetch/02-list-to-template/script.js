8cea7aa7163581ba039279535bdf52a8b63f6b01
sali <salialmurshidi@gmail.com>
2020-01-07 11:27:41 +0100

    done with this by do clone to template and but the element in it

--- 09-fetch/02-list-to-template/script.js
+++ 09-fetch/02-list-to-template/script.js
@@ -9,6 +9,41 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(() => {
-    // your code here
-})();
+// an object of the data in the json
+let dataNames;
+// the template id
+let temp = document.getElementsByTagName("template")[0];
+
+// your code here
+fetch('/api.json').then(response => {
+    return response.json();
+})
+    .then(function (data) {
+        dataNames = data.heroes;
+    })
+    .catch(err => {
+        console.log("error");
+    })
+
+// add event listener to the button
+document.getElementById("run").addEventListener("click", function () {
+    // test the array we can get it
+    console.log(dataNames[0].name);
+    // for each to get al elements in array
+    dataNames.forEach(function (element, index) {
+        // here make clone to the content in template to let it appearance
+        let clonData = temp.content.cloneNode(true);
+        // querySelector to get element in the clonData by class name and but value inside it
+        clonData.querySelector(".name").innerHTML = element.name;
+        clonData.querySelector(".alter-ego").innerHTML = element.alterEgo;
+        // add clonData in the target id to appearance there
+        document.getElementById("target").appendChild(clonData);
+    })
+
+});
+
+
+
+
+
+
