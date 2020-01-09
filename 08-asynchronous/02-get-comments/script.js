c731156b4d3ee55bb380386bde059253d42bd923
sali <salialmurshidi@gmail.com>
2020-01-09 12:30:39 +0100

    finished 8.2

--- 08-asynchronous/02-get-comments/script.js
+++ 08-asynchronous/02-get-comments/script.js
@@ -1,14 +1,23 @@
-/* becode/javascript
- *
- * /10-asynchronous/02-get-comments/script.js - 10.2: chargement d'articles et de commentaires
- *
- * coded by leny@BeCode
- * started at 09/05/2019
- */
+let dataArray;
+document.getElementById("run").addEventListener("click", function () {
+    window.lib.getPosts(function (error, data) {
 
-// NOTE: don't focus on the existing code structure for now.
-// You will have time to focus on it later.
+        dataArray = data;
+        forEachElements();
+    });
 
-(() => {
-    // your code here
-})();
+});
+
+function forEachElements() {
+   // console.log(dataArray);
+
+    dataArray.forEach(function (element, index) {
+
+        window.lib.getComments(null ,function (error, data) {
+           // console.log(data);
+            if (data[element.id] != undefined) {
+                console.log(data[element.id]);
+            }
+        });
+    });
+}
