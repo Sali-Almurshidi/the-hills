e8ae6366362254de9258152ff4ec57aef1c10c71
sali <salialmurshidi@gmail.com>
2020-01-22 18:55:25 +0100

    I like it

--- 08-asynchronous/06-promise-handling-errors/script.js
+++ 08-asynchronous/06-promise-handling-errors/script.js
@@ -1,14 +1,15 @@
-/* becode/javascript
- *
- * /10-asynchronous/06-promise-handling-errors/script.js - 10.6: gestion d'erreur (Promise)
- *
- * coded by leny@BeCode
- * started at 09/05/2019
- */
+document.getElementById("run").addEventListener("click", function () {
+    window.lib.getPersons()
+        .then(
+            function (resolve, reject) {
+                // dataArray = data;
+                if (resolve == undefined) {
+                    console.log("there is no data ");
+                    console.error();
+                } else {
+                    console.log(resolve);
+                }
+            }
+        ) // end then
 
-// NOTE: don't focus on the existing code structure for now.
-// You will have time to focus on it later.
-
-(() => {
-    // your code here
-})();
+});
