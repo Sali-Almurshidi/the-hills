693414186f7e0a08bca8974c9e99cdb83577edd4
sali <salialmurshidi@gmail.com>
2020-01-22 18:48:55 +0100

    done

--- 08-asynchronous/05-promise-get-comments/script.js
+++ 08-asynchronous/05-promise-get-comments/script.js
@@ -1,14 +1,27 @@
-/* becode/javascript
- *
- * /10-asynchronous/05-promise-get-comments/script.js - 10.5: chargement d'articles et de commentaires (Promise)
- *
- * coded by leny@BeCode
- * started at 09/05/2019
- */
 
-// NOTE: don't focus on the existing code structure for now.
-// You will have time to focus on it later.
 
-(() => {
-    // your code here
-})();
+//let dataArray;
+document.getElementById("run").addEventListener("click", function () {
+    window.lib.getPosts()
+        .then(json => forEachElements(json)); // call function to print getComments
+
+});
+
+function forEachElements(data) {
+    data.forEach(function (element) {
+        window.lib.getComments()
+            .then
+            (function (addComment) {
+                let comments = addComment[element.id];
+                if (comments !== undefined) {
+                    element.comment = comments;
+                }
+                console.log(element);
+            })
+    });
+}
+
+
+
+
+
