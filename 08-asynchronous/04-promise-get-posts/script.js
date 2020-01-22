475e3801c2a0d120f6eea60cc679b4c87417c08b
sali <salialmurshidi@gmail.com>
2020-01-22 18:34:25 +0100

    this is ok

--- 08-asynchronous/04-promise-get-posts/script.js
+++ 08-asynchronous/04-promise-get-posts/script.js
@@ -1,14 +1,6 @@
-/* becode/javascript
- *
- * /10-asynchronous/04-promise-get-posts/script.js - 10.4: chargement d'articles (Promise)
- *
- * coded by leny@BeCode
- * started at 09/05/2019
- */
 
-// NOTE: don't focus on the existing code structure for now.
-// You will have time to focus on it later.
+document.getElementById("run").addEventListener("click" , function () {
+    window.lib.getPosts() // getPosts with out parameter
+        .then(json => console.log(json)); // there is promises so we need then
+});
 
-(() => {
-    // your code here
-})();
