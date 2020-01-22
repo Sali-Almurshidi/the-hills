c696acc757543ef999adbd9b1a5a7bc0b9381fb8
sali <salialmurshidi@gmail.com>
2020-01-22 18:23:43 +0100

    what is going on

--- 08-asynchronous/03-handling-errors/script.js
+++ 08-asynchronous/03-handling-errors/script.js
@@ -1,14 +1,11 @@
-/* becode/javascript
- *
- * /10-asynchronous/03-handling-errors/script.js - 10.3: gestion d'erreur
- *
- * coded by leny@BeCode
- * started at 09/05/2019
- */
 
-// NOTE: don't focus on the existing code structure for now.
-// You will have time to focus on it later.
-
-(() => {
-    // your code here
-})();
+document.getElementById("run").addEventListener("click" , function () {
+    window.lib.getPersons(function (error, data) {
+        // dataArray = data;
+       if(data == undefined){
+           console.log("there is no data ");
+       }else {
+           console.log(data);
+       }
+    });
+});
