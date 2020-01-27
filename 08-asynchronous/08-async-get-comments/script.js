a533d13679c3731d179a756aa5c5758e7fcb22bc
sali <salialmurshidi@gmail.com>
2020-01-27 17:24:54 +0100

    done

--- 08-asynchronous/08-async-get-comments/script.js
+++ 08-asynchronous/08-async-get-comments/script.js
@@ -1,14 +1,27 @@
-/* becode/javascript
- *
- * /10-asynchronous/08-async-get-comments/script.js - 10.8: chargement d'articles et de commentaires (async/await)
- *
- * coded by leny@BeCode
- * started at 09/05/2019
- */
 
-// NOTE: don't focus on the existing code structure for now.
-// You will have time to focus on it later.
 
-(() => {
-    // your code here
-})();
+document.getElementById("run").addEventListener("click", async function () {
+    const posts = await window.lib.getPosts();
+    forEachElements(posts);
+});
+
+function forEachElements(data) {
+    data.forEach(async function (element) {
+        const comments = await window.lib.getComments();
+
+            add(comments);
+
+            function add (addComment) {
+                let comments = addComment[element.id];
+                if (comments !== undefined) {
+                    element.comment = comments;
+                }
+                console.log(element);
+            }
+    });
+}
+
+
+
+
+
